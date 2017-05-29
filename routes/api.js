ae78999064eab6dc92d19c3043c8c127124106e4
Faique <faiquemoqeet@gmail.com>
2017-05-28 23:56:40 -0500

    Some Progress

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/:resource', function(req, res, next){
+
+    var resource = req.params.resource
+
+    res.json({
+        confirmation: 'sucess',
+        resource: resource
+    })
+})   
+
+module.exports = router;
